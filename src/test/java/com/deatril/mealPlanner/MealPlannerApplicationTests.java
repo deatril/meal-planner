a7e3c9754ba2958459ff3a485d3337f3e80284a0
Antony LE COQ <antony.lecoq@free.fr>
2022-05-02 20:10:07 +0200

    init project

---
+++ src/test/java/com/deatril/mealPlanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deatril.mealPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
